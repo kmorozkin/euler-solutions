721ad91bfe0b740faa7bb08eae4a29953d8d9509
kmorozkin <kmorozkin@luxoft.com>
2018-10-23 17:57:33 +0300

    Problem 60 solved

--- src/problem60.py
+++ src/problem60.py
@@ -5,10 +5,12 @@ result will always be prime. For example, taking 7 and 109, both 7109 and 1097 a
 
 Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 '''
+from src.utils.decorators import Memoize
 from src.utils.primes import gen_primes, is_probable_prime
 from itertools import permutations
 
 
+@Memoize
 def is_prime_pair(item1, item2):
     juxtaposed = ('%d%d' % permutation_tuple for permutation_tuple in permutations((item1, item2), 2))
     return all(map(is_probable_prime, map(int, juxtaposed)))
@@ -16,18 +18,32 @@ def is_prime_pair(item1, item2):
 
 class Collector:
     def __init__(self):
-        self.cache_dictionary = dict()
         self.max_pair_num = 0
+        self.cache = []
 
-    def add_pair(self, item1, item2):
-        for pair in ((item1, item2), (item2, item1)):
-            cache = self.cache_dictionary.setdefault(pair[0], {pair[0]})
-            if all(is_prime_pair(x, pair[1]) for x in cache):
-                cache.add(pair[1])
-                self.max_pair_num = max(len(cache), self.max_pair_num)
+    def add(self, *items):
+        def all_match(coll1, coll2):
+            for other_item in coll1:
+                for cache_item in coll2:
+                    if not is_prime_pair(cache_item, other_item):
+                        return False
+            return True
+
+        def update():
+            items_list = list(items)
+            for i, item in enumerate(items_list):
+                rest = [v for j, v in enumerate(items_list) if j != i]
+                for cache_set in self.cache:
+                    if item in cache_set and all_match(rest, cache_set):
+                        cache_set.update(rest)
+                        self.max_pair_num = max(len(cache_set), self.max_pair_num)
+                        return True
+            return False
+        if not update():
+            self.cache.append(set(items))
 
     def pairs(self, size):
-        return [v for v in self.cache_dictionary.values() if len(v) == size]
+        return [v for v in self.cache if len(v) == size]
 
 
 limit = 5
@@ -37,7 +53,7 @@ generated = [next(iterator)]
 for prime_x in iterator:
     for prime_y in generated:
         if is_prime_pair(prime_y, prime_x):
-            collector.add_pair(prime_y, prime_x)
+            collector.add(prime_y, prime_x)
     generated.append(prime_x)
     if collector.max_pair_num >= limit:
         current_pairs = collector.pairs(limit)
@@ -45,14 +61,4 @@ for prime_x in iterator:
             print(group)
             print(sum(group))
         exit()
-
-
-# primes_list = list(islice(gen_primes(), 0, 700))
-# for i in range(len(primes_list)):
-#     for j in range(i, len(primes_list)):
-#         prime_x = primes_list[i]
-#         prime_y = primes_list[j]
-#         if is_prime_pair(prime_x, prime_y):
-#             collector.add_pair(prime_x, prime_y)
-# print(collector.pairs(5))
 
